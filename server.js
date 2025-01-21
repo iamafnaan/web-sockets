bca032a59c8dfb66c4a2bf3a3fe5343479944f36
iamafnaan <afnaanahmed33@gmail.com>
2025-01-21 19:38:29 +0530

    server side socket creation

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const Websocket = require('ws');
+
+const app = express();
+const PORT = 3000;
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+const wss = new Websocket.server({server});
+
+wss.on('connection', (ws) =>{
+    console.log('client is connected');
+
+    ws.on('message received', (message) =>{
+        console.log(`message received , ${message}`);
+    })
+
+    ws.forEach((client) => {
+        if (client.readyState ===Websocket.OPEN){
+            client.send(message); 
+        }
+    })
+
+    ws.on('close', () => {
+        console.log(`client is disconnected`);
+    })
+})
